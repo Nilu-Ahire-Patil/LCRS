0e9f0a1879d63aec806c347b22bfdd4cb89e25bd
Nilu-Ahire-Patil <nilesh909697@gmail.com>
2024-11-10 19:33:11 +0530

    added macro to set spdlog log level at compile time

--- lcrs/cmake/spdlog.cmake
+++ lcrs/cmake/spdlog.cmake
@@ -2,6 +2,8 @@ include(FetchContent)
 
 find_package(spdlog QUIET)
 
+add_compile_definitions(SPDLOG_ACTIVE_LEVEL=0)
+
 if (NOT spdlog_FOUND)
     option(SPDLOG_BUILD_SHARED "build spdlog as shared library" ${BUILD_SHARED_LIBS})
 
