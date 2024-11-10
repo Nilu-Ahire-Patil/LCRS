6623daee94a3a7f43ff870be1979e74b449840c5
Nilu-Ahire-Patil <nilesh909697@gmail.com>
2024-11-10 14:20:06 +0530

    added Test class to test the classes and modules, in future this clss will using GTEST to test classes and modules

--- lcrs/src/lcrsTest.cpp
+++ lcrs/src/lcrsTest.cpp
@@ -2,27 +2,37 @@
 #include <sstream>
 #include <string>
 
-#define SPDLOG_ACTIVE_LEVEL 0
+#include "lcrs/logger.h" // Always include first
 
-#include "lcrs/logger.h"
+#include "lcrs/system.h"
 
-// int main() {
-//     listMacAddresses();
-//     return 0;
-// }
+class Test : public System
+{
+public:
+    static int system()
+    {
+        return System::getMacAddressMap().empty() ? EXIT_FAILURE : EXIT_SUCCESS;
+    }
 
-int main() {
+    static int logger()
+    {
+        SPDLOG_TRACE("this is trace message");
+        SPDLOG_ERROR("this is error message");
 
-    Logger::init("lcrsTest");
-    Logger::setLevel(spdlog::level::trace);
+        SPDLOG_INFO("info");
 
-    SPDLOG_TRACE("this is trace message");
-    SPDLOG_ERROR("this is error message");
+        lcrsLog(INFO, "thsi is trace message {}", 3);
+        lcrsLog(INFO, "thsi is trace message");
+        lcrsLog(TRACE, "thsi is trace message");
 
-    SPDLOG_INFO("info");
-   
-    lcrsLog(INFO, "thsi is trace message {}", 3);
-    lcrsLog(INFO, "thsi is trace message");
+        return EXIT_SUCCESS;
+    }
+};
 
-    return 0;
+int
+main()
+{
+    Logger::init("lcrsTest", spdlog::level::trace);
+    // loggerTest();
+    return Test::system();
 }
